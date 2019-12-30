b2faf17d732300a46e35dfeb881a55f7f4ae1a85
Ron Masuary <ronmas2@gmail.com>
2019-12-30 20:57:00 +0200

    Updates

---
+++ precache-manifest.1c3c26dbccd02b996868bc34003f741f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c73ebb67b6af9c0dfbbb4fabf51fe1b",
+    "url": "/ex/index.html"
+  },
+  {
+    "revision": "4b228820bca8fe677aab",
+    "url": "/ex/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "484924c17bf769b878ba",
+    "url": "/ex/static/css/main.9a52309d.chunk.css"
+  },
+  {
+    "revision": "4b228820bca8fe677aab",
+    "url": "/ex/static/js/2.89afa8bf.chunk.js"
+  },
+  {
+    "revision": "484924c17bf769b878ba",
+    "url": "/ex/static/js/main.9462625e.chunk.js"
+  },
+  {
+    "revision": "7f8f93b648903d5306a5",
+    "url": "/ex/static/js/runtime-main.53d31c66.js"
+  },
+  {
+    "revision": "905e61f43a86ca7735a27eeaa9386805",
+    "url": "/ex/static/media/flower.905e61f4.jfif"
+  },
+  {
+    "revision": "3b18036ee35b5d86495fb9e49071ba65",
+    "url": "/ex/static/media/mountains.3b18036e.jfif"
+  }
+]);
